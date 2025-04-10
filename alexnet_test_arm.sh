9d2a26bd63fd68453b73e3abae7de95c30c9077d
luc <luc@forgotten.name>
2025-04-10 14:33:51 -0700

    feat: added alexnet test arm script

--- alexnet_test_arm.sh
+++ alexnet_test_arm.sh
@@ -2,12 +2,12 @@
 # ARM version of alexnet_test.sh
 
 # Rebuild with ARM optimizations
-echo "Rebuilding for ARM architecture..."
-./build_arm.sh
+#echo "Rebuilding for ARM architecture..."
+# ./build_arm.sh
 
 echo "Running AlexNet tests on ARM..."
 cd build_arm/src
 ./main mAlexNet 4 1 4 1
 ./main mAlexNet 8 1 4 1
 ./main mAlexNet 16 1 4 1
-cd ../.. 
+cd ../..
